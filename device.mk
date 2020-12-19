ee7f8c15915fe4127c71a0ef8d0dc3e04b854b18
Aghora7 <ukikieran@gmail.com>
2020-12-19 17:30:32 +0530

    [1/1] This may fix sensor related issues

--- device.mk
+++ device.mk
@@ -192,6 +192,7 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
+    $(LOCAL_PATH)/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
 
 # WiFi
 PRODUCT_COPY_FILES += \
